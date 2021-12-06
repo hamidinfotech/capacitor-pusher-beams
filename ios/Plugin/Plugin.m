9ba2a97cca698915291e0ea51a6cab21edad03fe
Hamid reza Hosseini <hamid.infotech@gmail.com>
2021-12-06 18:37:44 +0100

    add pusher start for iOS

--- ios/Plugin/Plugin.m
+++ ios/Plugin/Plugin.m
@@ -4,6 +4,7 @@
 // Define the plugin using the CAP_PLUGIN Macro, and
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(PusherBeams, "PusherBeams",
+           CAP_PLUGIN_METHOD(start, CAPPluginReturnPromise);        
            CAP_PLUGIN_METHOD(addDeviceInterest, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(removeDeviceInterest, CAPPluginReturnPromise);
            CAP_PLUGIN_METHOD(getDeviceInterests, CAPPluginReturnPromise);
